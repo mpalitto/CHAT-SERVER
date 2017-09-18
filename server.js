8c4758546ac31523e1b20caa58b739391714d49c
Matteo <mpalitto@gmail.com>
2017-09-18 10:12:44 +0200

    added sqlite + create invitaions DB + anyInvitation request + new invitation query

--- server.js
+++ server.js
@@ -2,6 +2,53 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+
+let sql = 'CREATE TABLE IF NOT EXISTS INVITATIONS ( userID TEXT PRIMARY KEY NOT NULL, invites TEXT NOT NULL)';
+const sqlite3 = require('sqlite3').verbose();
+let invitationsDB = new sqlite3.Database('./db/invitations.db', (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the INVITATIOS SQlite database.');
+});
+invitationsDB.serialize(function() {
+  invitationsDB.run(sql, [], function(err) {
+    if (err) {
+      console.log('Error executing statement:', err, err.stack);
+      //callback(err);
+      return;
+    }
+    console.log('INVITATION DB created successfully!');
+    sql = 'INSERT OR IGNORE INTO INVITATIONS (userID, invites) VALUES ("mpalitto@gmail.com", "{wife@gmail.com, FAMILY-BUDGET}")';
+    invitationsDB.run(sql, [], function(err) {
+      if (err) {
+        console.log('Error executing statement:', err, err.stack);
+        //callback(err);
+        return;
+      }
+      console.log('first INVITATION ADDED successfully');
+    });
+    sql = 'UPDATE INVITATIONS SET invites = invites || ", {son@gmail.com, FAMILY-BUDGET}" WHERE invites NOT LIKE "%{son@gmail.com, FAMILY-BUDGET%"';
+    invitationsDB.run(sql, [], function(err) {
+      if (err) {
+        console.log('Error executing statement:', err, err.stack);
+        //callback(err);
+        return;
+      }
+      console.log('new INVITATION ADDED successfully');
+    });
+    sql = 'SELECT * FROM INVITATIONS ORDER BY userID';
+    invitationsDB.each(sql, [], function(err, row) {
+      if (err) {
+        console.log('Error executing statement:', err, err.stack);
+        //callback(err);
+        return;
+      }
+      console.log(`${row.userID} - ${row.invites}`);
+    });
+  });
+});
+
 users = [];
 connections = [];
 
@@ -9,7 +56,8 @@ server.listen(process.env.PORT || 3000);
 
 //app.get();
 
-io.sockets.on('connection', function(socket){
+//all apps connect to the initial-connection for sending and receiving set-up communications
+io.on('connection', function(socket){
   connections.push(socket);
   console.log('active connections: ' + connections.length);
 
@@ -17,6 +65,32 @@ io.sockets.on('connection', function(socket){
   socket.on('disconnect', function(data){
     connections.splice(connections.indexOf(socket), 1);
     console.log('Disconnected, active connectionis remaining: ' + connections.length)
+  });
+
+  //first thing the app will send the userID and the groups it wants to join
+  socket.on('user', function(user) {
+    //join groups
+    socket.join(user.groups);
+    //check for invitations
+  });
+
+  //check request if there are any invites for client
+  socket.on('anyInvites', function(user){
+    console.log('new invites check request from: '+user.userID);
+    sql = `SELECT userID userID,
+      INVITES invites
+      FROM INVITATIONS
+      WHERE userID  = ?`;
+    sql = 'SELECT * FROM INVITATIONS WHERE userID = ?';
+    invitationsDB.each(sql, [user.userID], (err, row) => {
+      if (err) {
+        return console.error(err.message);
+      }
+      return row
+         ? console.log('FOUND! ', row.userID, row.invites)
+         : console.log(`No INVITE found for: ${user.userID}`);
+     
+    });    
   });
 
   //getting data from a client
